cf7a845a71ef25540b3f42d4635ca9a0131dd39a
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-29 02:21:38 +0600

    Done

---
+++ driving-test-if-else.py
@@ -0,0 +1,12 @@
+print("Enter Your Age :")
+
+age = int(input())
+
+if age < 18:
+    print("Sorry! You Can't Drive")
+
+elif age == 18:
+    print("We Will think about you!")
+
+else:
+    print("Congratulations! You can Drive!!!!")
