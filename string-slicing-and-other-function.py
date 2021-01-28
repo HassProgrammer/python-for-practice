0d5bf998b48e56b416f116fec90d38cd644d88e7
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-28 22:52:52 +0600

    string-slicing-and-other-function done

---
+++ string-slicing-and-other-function.py
@@ -0,0 +1,17 @@
+mystr  = "Khan is a good boy"
+print(len(mystr))
+print(mystr[0:16])
+print(mystr[0:16:2])
+print(mystr[:])
+
+###############################
+
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.endswith("boy"))
+print(mystr.count("b"))
+print(mystr.capitalize())
+print(mystr.find("is"))
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.replace("is", "the"))
