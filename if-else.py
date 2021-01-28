7f8d8b16a49a053ad7a919887a8ed34a215ac0a2
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-29 02:14:05 +0600

    ok

---
+++ if-else.py
@@ -0,0 +1,17 @@
+var1 = 6
+var2 = 56
+
+#var3 = int(input())
+
+if var3 > var2:
+    print("Greater")
+elif var3 == var2:
+     print("Equal") 
+else:
+    print("Lesser")
+
+list1 = [5, 7, 3]
+if 5 in list1:
+    print("ok")
+if 15 not in list1:
+    print("404")
