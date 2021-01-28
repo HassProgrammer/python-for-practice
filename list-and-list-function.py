24569cdac74e1a08ac5dddb088a677ac968bce9f
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-28 23:31:42 +0600

    Done

---
+++ list-and-list-function.py
@@ -0,0 +1,30 @@
+grocery = ["Harpic", "Vim bar", "Deodrant", "Rice", "Lollypoop", 56]
+print(grocery)
+print(grocery[4])
+
+numbers = [2, 7, 9, 11, 3]
+numbers.sort()
+print(numbers)
+print(numbers[2])
+numbers.reverse()
+print(numbers)
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+numbers.append(7)
+print(numbers)
+numbers.insert(4, 67)
+print(numbers)
+numbers.remove(9)
+print(numbers)
+numbers.pop()
+print(numbers)
+
+tp = (1, 2, 3)
+print(tp)
+
+#Swaping
+a = 1
+b = 2
+a,b = b,a
+print(a,b)
