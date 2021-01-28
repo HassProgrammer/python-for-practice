7cb525be9d5394b169d00cc7cfadcbc0b437d699
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-29 01:35:26 +0600

    done

---
+++ sets.py
@@ -0,0 +1,13 @@
+s = set()
+print(type(s))
+s_from_list = set([1, 2, 3, 4])
+print(s_from_list)
+
+s.add(1)
+s.add(1)
+s.add(2)
+s1 = s.union({1, 2, 3})
+print(s)
+print(s,s1)
+s.remove(2)
+print(s)
