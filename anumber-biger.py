ea43ca5ef11dd781859fcfb51c6c6a864e4553af
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-31 02:00:47 +0600

    ok

---
+++ anumber-biger.py
@@ -0,0 +1,8 @@
+while(True):
+    inp = int(input("Enter a number:"))
+    if inp>100:
+        print("congratulation you have entered a number greater then 100")
+        break
+    else:
+        print("Try again!")
+        continue
