6165c555cb03d698c6439c0be34b3ba7357c9c9a
HassProgrammer <md.ahshanulalam@gmail.com>
2021-02-02 15:43:58 +0600

    Done

---
+++ try_except.py
@@ -0,0 +1,11 @@
+print("Enter Num 1:")
+num1 = input()
+print("Enter Num 2:")
+num2 = input()
+
+try:
+    int(num1) + int(num2)
+except Exception as e:
+    print(e)
+
+print("This line is very important")
