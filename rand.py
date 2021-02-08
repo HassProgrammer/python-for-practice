d3529b0e9c7f45426612d19e49480d9f7881d9f4
HassProgrammer <md.ahshanulalam@gmail.com>
2021-02-09 03:46:46 +0600

    ok bro

---
+++ rand.py
@@ -0,0 +1,12 @@
+import random
+
+random_number = random.randint(0, 6)
+rand = random.random()
+rand2 = random.random()*100
+print(random_number)
+print(rand)
+print(rand2)
+
+list = ["E tv", "CN", "Poogo","Pice TV"]
+choice = random.choice(list)
+print(choice)
